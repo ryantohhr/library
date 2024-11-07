16195504bc5d45a2b3cb30f577638ce56e20263e
ryantohhr <ryantohhongrui@hotmail.com>
2024-11-07 14:11:50 +0800

    Implemented Book and addBookToLibrary

---
+++ script.js
@@ -0,0 +1,31 @@
+const Library = [];
+
+function Book(title, author, read) {
+    this.title = title,
+    this.author = author,
+    this.read = read
+}
+
+function addBookToLibrary(title, author, read) {
+    let book = new Book(title, author, read);
+    Library.push(book);
+}
+
+function displayBook(book) {
+    const card = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("div");
+    const main = document.querySelector(".main");
+    console.log(Library);
+    title.textContent = book.title;
+    author.textContent = book.author;
+    card.appendChild(title);
+    card.appendChild(author);
+    main.appendChild(card);
+}
+
+function displayLibrary() {
+    for (book in Library) {
+        displayBook(book);
+    }
+}
